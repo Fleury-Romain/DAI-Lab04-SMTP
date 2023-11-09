ab01d2af5ebe23b1e1f8426855b6734b5f2a34ef
Fleury-Romain <romain.fleury@heig-vd.ch>
2023-11-09 14:05:03 +0100

    Test gitignore

--- code/src/main/java/ch/heig/dai/lab/SMTP/MainTest.java
+++ code/src/main/java/ch/heig/dai/lab/SMTP/MainTest.java
@@ -3,5 +3,6 @@ package ch.heig.dai.lab.SMTP;
 public class MainTest{
     public static void main(String[] args){
         System.out.println("Hello world !");
+        System.out.println("How are you sunshine ?");
     }
 }
