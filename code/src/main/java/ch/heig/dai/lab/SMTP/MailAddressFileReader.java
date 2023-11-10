0f63850f348c9d2c51a45ef4840517316a2936e6
Julien Billeter, PhD, Ing. M.Sc., M.B.A. <julien.billeter@alumni.ethz.ch>
2023-11-10 08:25:16 +0100

    MAJ MailAddressFileReader.java

--- code/src/main/java/ch/heig/dai/lab/SMTP/MailAddressFileReader.java
+++ code/src/main/java/ch/heig/dai/lab/SMTP/MailAddressFileReader.java
@@ -1,15 +1,52 @@
 package ch.heig.dai.lab.SMTP;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class MailAddressFileReader {
     private final BufferedReader file;
-
     public MailAddressFileReader(String filePath) throws FileNotFoundException {
         file = new BufferedReader(new FileReader(filePath));
     }
 
-    public String[] getAddresses() {
+    public ArrayList<String> getAddress() {
+        /*
+         read line per line file, detect # as the beginning of a new group and what follows the # sign as the name of the group, sender: and what
+         follows as the e-mail address of the sender, and the each line starting with receiver: and what
+         follows as the e-mail address of the receiver (one receiver per line).
+         finish when a line with only \n is found
+         */
+        System.out.println("MailAddressFileReader");
+        ArrayList<String> array = new ArrayList<>();
+        String line;
+        int i = 0;
+        while(true){
+            try {
+                if ((line = file.readLine()) == null) break;
+                if(line.equals("\n")) break;
+                if(line.contains("#")){
+                    array.add(line.split("#")[1]);
+                    System.out.println("group: " + line);
+                }
+                if(line.contains("sender:")){
+                    array.add(line.split("sender:")[1]);
+                    System.out.println("sender: " + line);
+                }
+                if(line.contains("receiver:")){
+                    array.add(line.split("receiver:")[1]);
+                    System.out.println("receiver: " + line);
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return array;
+    }
+
+
+    /*
+    public String[] getAddress() {
          String[] mail;
          String line;
          while(true){
@@ -20,14 +57,8 @@ public class MailAddressFileReader {
                  throw new RuntimeException(e);
              }
         }
-
-         /*
-        public String[] getMail(int id){
-
-        }
-        */
         return new String[]{"salut", "c'estcool"};
-        // c'est tout bon merci, JB: ok
     }
+    */
 
 }
--- code/src/main/java/ch/heig/dai/lab/SMTP/MainTest.java
+++ code/src/main/java/ch/heig/dai/lab/SMTP/MainTest.java
@@ -1,5 +1,7 @@
 package ch.heig.dai.lab.SMTP;
 
+import java.io.FileNotFoundException;
+
 public class MainTest{
     public static void main(String[] args){
         try (
@@ -9,8 +11,10 @@ public class MainTest{
             MailContent mc = new MailContent(mcfr.getMail());
             MailAddress ma = new MailAddress(3, "romain.fleury@heig-vd.ch", new String[]{"t411galas@gmail.com", "r.fleury.1400@gmail.com"});
 
-            ConnectionHandler ch = new ConnectionHandler("localhost", 1025, ma, mc);
-            ch.run();
+        MailContentFileReader mcfr = null;
+        try {
+            mcfr = new MailContentFileReader("C:\\HEIG\\Semestre 3\\DAI\\Laboratoire\\DAI-Lab04-SMTP\\Data\\MailingCore.txt");
+            String[] test = mcfr.getMail();
 
         } catch (Exception e) {
             throw new RuntimeException(e);
